07ef3e14c11f355cc5e60a3378269e56a652c672
d0ngle8k <trggiathanh2003@gmail.com>
2025-10-17 04:05:01 +0700

    New: Add new image and information about primitive data type, code testing adn deployment

--- Solidity Code/s1mple storage/contracts/S1mpleStorage.sol
+++ Solidity Code/s1mple storage/contracts/S1mpleStorage.sol
@@ -3,10 +3,37 @@ pragma solidity ^0.8.19;
 
 contract SimpleStorate{
     // gg
-    uint256 public favoriteNumber; // default 0
+    uint256 myfavoriteNumber; // default 0
+    // uint256[] listofFavoriteNumbers ;  // create array
+
+    struct Person{ // define a struct
+        uint256 favNumbs;
+        string name;
+    }
+    // Person public myniqqa = Person (7, "CJ") {
+        
+    // }
+
+    // Person public mynigga = Person({favNumbs: 69, name: "Claude"});
+    // Person public cj = Person({favNumbs: 69, name: "Claude"});
+    // Person public claude = Person({favNumbs: 69, name: "Claude"});
+    
+    // Person[add_your_value_here] public listOfPeople; // define a static array 
+
+    Person[] public listOfPeople; // define a dynamic array
+    mapping(string => uint256) public nameToFavorNumber;
 
     function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
+        myfavoriteNumber = _favoriteNumber;
     }
-    //0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8
+    function retrieve() public view returns(uint256){
+        return myfavoriteNumber;
+    }
+    function addPerson(string calldata _name, uint256 _favoriteNumber) public {
+        // Person memory newPerson = Person(_favoriteNumber, _name);
+        // listOfPeople.push(newPerson);
+        listOfPeople.push(Person(_favoriteNumber, _name));
+        nameToFavorNumber [_name] = _favoriteNumber;
+
+    }
 }
